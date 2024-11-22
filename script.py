a6a1f7c275e9276a4908829139d915836f9affc1
Gabriel <g.chaves0110@gmail.com>
2024-11-22 14:41:29 -0300

    :sparkles: feat: Coletar informações das citações

--- script.py
+++ script.py
@@ -11,12 +11,30 @@ def open_page(driver, url):
     driver.get(url)
     time.sleep(10)
 
+def get_all_quotes(driver):
+    quotes_data = []
+    quotes = driver.find_elements(By.CLASS_NAME, "quote")
+
+    for quote in quotes:
+        text = quote.find_element(By.CLASS_NAME, "text").text 
+        author = quote.find_element(By.CLASS_NAME, "author").text 
+        tags = [tag.text for tag in quote.find_elements(By.CLASS_NAME, "tag")]
+        quotes_data.append({"Citação": text, "Autor": author, "Tags": tags} )
+
+    return quotes_data
+
 def main():
     url = "https://quotes.toscrape.com/js-delayed/"
     driver = setup_driver()
 
     try:
         open_page(driver, url)
+        quotes = get_all_quotes(driver)
+        for quote in quotes:
+            print(f"Citação: {quote['Citação']}")
+            print(f"Autor: {quote['Autor']}")
+            print(f"Tags: {quote['Tags']}")
+            print("-" * 40)
     finally:
         driver.quit()
 
